492e8766dd06db492c163bef11f3f957cbe22ef0
Dawid Gorczyca <Dawid Gorczyca>
2017-03-08 17:07:01 +0100

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import { mount, shallow } from 'enzyme';
 import sinon from 'sinon';
 import App from './App';
 
+// THIS IS ONLY A MOCKUP, would still need to set up the state for tests and proper rendering
 
 describe('<App />', () => {
 
